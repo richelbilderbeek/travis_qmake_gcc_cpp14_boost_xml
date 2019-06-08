e78a6c5f7eb33c36a0e1fdeff2d54e625d8b5f21
richelbilderbeek <richel@richelbilderbeek.nl>
2019-06-08 08:31:07 +0200

    Add XML file

--- main.cpp
+++ main.cpp
@@ -23,9 +23,9 @@ namespace pt = boost::property_tree;
 //[debug_settings_data
 struct debug_settings
 {
-    std::string m_file;               // log filename
-    int m_level;                      // debug level
-    std::set<std::string> m_modules;  // modules where logging is enabled
+    std::string m_file = {};              // log filename
+    int m_level = {};                     // debug level
+    std::set<std::string> m_modules = {}; // modules where logging is enabled
     void load(const std::string &filename);
     void save(const std::string &filename);
 };
